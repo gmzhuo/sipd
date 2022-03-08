5c96866ccbd0039223cc15c7014979018f538749
gmzhuo <18414710@qq.com>
2022-03-08 16:26:04 +0800

    Update siprouter.cpp

--- src/siprouter.cpp
+++ src/siprouter.cpp
@@ -3,33 +3,37 @@
 void SIPRouter::forwardMessage(const SIPEndpoint& from, const SIPMessage& message)
 {
   std::string target = message.getDestCommunication();
-  auto it = m_endpointsMapByCommunication.find(target);
+  std::string ua = message.getDestTarget();
+  auto it = m_endpoints.find(ua);
+
   if(it != m_endpointsMapByCommunication.end()) {
-    it->second->sendMessage(message);
+    auto eit = it->second->find(target);
+    if(eit != it->second->end()) {
+      eit->second->sendMessage(message);
+    }
   }
 }
 
 
 void SIPRouter::forwardSolicit(const SIPEndpoint& from, const SIPMessage& message)
 {
-  std::string target = message.getDestTarget();
-  auto it = m_endpointsMapByName.find(target);
-  if(it != m_endpointsMapByName.end()) {
-    for(auto eip = it->second->begin(); eip != it->second->end; ++eip) {
-      (*eip)->sendMessage(message);
+  std::string ua = message.getDestTarget();
+  auto it = m_endpoints.find(ua);
+  if(it != m_endpoints.end()) {
+    for(auto eip = it->second->begin(); eip != it->second->end(); ++eip) {
+      eip->second->sendMessage(message);
     }
   }
 }
 
 void SIPRouter::registerEndpoint(std::shared_ptr<SIPEndpoint> &ep, const std::string& ua, const std::string& communication)
 {
-  m_endpointsMapByCommunication[communication] = ep;
-  auto it = m_endpointsMapByName.find(ua);
-  if(it != m_endpointsMapByName.end()) {
-    it->second.push_back(ep);
+  auto it = m_endpoints.find(ua);
+  if(it != m_endpoints.end()) {
+    it->second[communication] = ep;
   } else {
-    std::list<std::shared_ptr<SIPEndpoint>> epl;
-    epl.push_back(ep);
-    m_endpointsMapByName[ua] = epl;
+    std::map<std::string std::shared_ptr<SIPEndpoint>> eps;
+    eps[communication] = ep;
+    m_endpoints[ua] = eps;
   }
 }
