75fb19c2cff8ffa1a5f06b7ac58284dc783a9a2d
gmzhuo <18414710@qq.com>
2022-03-08 11:05:15 +0800

    Update sipmessage.h

--- src/sipmessage.h
+++ src/sipmessage.h
@@ -8,4 +8,7 @@ public:
   virtual bool isSolicit() const;
   const std::string& getDestCommunication() const;
   const std::string& getDestTarget() const;
+private:
+  std::string m_destCommunication;
+  std::string m_destTarget;
 };
