4ad9b35ca0d225c291032d446c25bdbbef9f4814
gmzhuo <18414710@qq.com>
2022-03-08 12:08:58 +0800

    Update sipmessage.h

--- src/sipmessage.h
+++ src/sipmessage.h
@@ -9,6 +9,12 @@ public:
   const std::string& getDestCommunication() const;
   const std::string& getDestTarget() const;
 private:
+  std::string m_method;
+  std::string m_ua;
+  std::string m_ruri;
+  std::string m_body;
+  std::map<std::string, std::string> m_headers;
+  std::map<std::string, std::string> m_extraHeaders;
   std::string m_destCommunication;
   std::string m_destTarget;
   bool m_isSolicit;
